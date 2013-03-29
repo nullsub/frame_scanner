d0d09208641db122f97f7ad9ef545dd8ea21330e
chrisu <s3066193@zih-tu.dresden.de>
2013-03-29 11:44:18 +0100

    merge scripts. remove unused converter

--- create_dxf.py
+++ create_dxf.py
@@ -4,8 +4,25 @@ from dxfwrite import DXFEngine as dxf
 import sys
 import getopt
 import os
+import math
+
+crrnt_angle = 0
+x_transformation = 0
+step_angle = 0.833333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333334
+
+def convert_to_coords(distance):
+	
+	print "dist is ", distance	
+
+	x = -math.sin(crrnt_angle*(math.pi/180))*distance
+	print "x is ", x	
+	y = math.cos(crrnt_angle*(math.pi/180))*distance
+	print "y is ", y	
+	x += x_transformation
+	return (x,y)
 
 def main(argv):
+	global crrnt_angle
 	inputfile = ''
 	outputfile = ''
 	try:
@@ -33,22 +50,30 @@ def main(argv):
 		print 'can not open input file', inputfile
 		sys.exit(2)
 
-	drawing = dxf.drawing(outputfile)
-
 	 #read all the lines into a list.
 	lines = f.readlines()	
 
 	if len(lines) <= 3:
 		print 'error: not enough points in ', inputfile
 		sys.exit(2)
+	
+	coords = []
+
+	for line in lines:
+		distance = int(line)
+		(x,y) = convert_to_coords(distance)	
+		crrnt_angle += step_angle
+		coords.append(str(int(x)) + " " + str(int(y)))
+	
+	drawing = dxf.drawing(outputfile)
 
 	prev_x = 0
 	prev_y = 0
 	start = 1
 	start_x = 0
 	start_y = 0
-	for line in lines:
-		numbers = line.split()
+	for coord in coords:
+		numbers = coord.split()
 		x = numbers[0]
 		y = numbers[1]
 		
